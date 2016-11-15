7a5abd97178aa0fc9cb8ff1e84275cc4fd940654
prototux <jason@prototux.net>
2016-11-15 13:24:30 +0100

    Reworked where statement

--- orange.sh
+++ orange.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
 
 ## Get JSON (with correct Referer)
-function getJson {
+getJson() {
 	echo $(curl -sS -H "Referer: http://couverture-mobile.orange.fr/mapV3/fibre/index.html" $1)
+}
+
+## Urlencode (from https://gist.github.com/cdown/1163649)
+urlencode() {
+	old_lc_collate=$LC_COLLATE
+	LC_COLLATE=C
+	local length="${#1}"
+	for (( i = 0; i < length; i++ )); do
+		local c="${1:i:1}"
+		case $c in
+			[a-zA-Z0-9.~_-]) printf "$c" ;;
+			*) printf '%%%02X' "'$c" ;;
+		esac
+	done
+	LC_COLLATE=$old_lc_collate
 }
 
 ## Read token from tmp file, if it exists, else get it and create the file
@@ -16,13 +31,10 @@ fi
 
 ## Parameters
 spatialref="3857" # Default: 102100
-
-# Cook parameters for the GET request
-where="(etape%3D%270%27%20AND%20sous_etape%20not%20in%20(%27A%27%2C%27B%27))%20or%20(etape%3C%3E%270%27)%20or%20(etape%3D%27%27%20AND%20sous_etape%20not%20in%20(%27A%27%2C%27B%27))"
+where=$(urlencode "(etape<>'0') or ((etape='0' or etape='') and sous_etape not in ('A','B'))")
 geometry="$1,$2,$3,$4" # xmin,ymin,xmax,ymax
 
 # Read http://resources.arcgis.com/en/help/rest/apiref/query.html for info on this
 request_url="http://couverture-mobile.orange.fr/arcsig/rest/services/extern/optimum_ftth/MapServer/0/query?"
 request_url+="token=$token&f=json&where=$where&geometry=$geometry&inSR=$spatialref&ourSR=$spatialref&outFields=*"
-
 echo $(echo $(getJson $request_url) | jq ${5:-.})
